4e3fe21222d177fe96fc725be782b9510a634f91
Angel <angel.pashov@proton.me>
2024-10-14 19:52:10 +0300

    Header component created

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { FaYoutube } from "react-icons/fa";
+import { HeaderMoreSection, LeftSection, LogoSection, SearchSection, StyledHeader } from "./Header.styles";
+import { SlMenu } from "react-icons/sl";
+import { Text } from "../../utils/text.styles";
+import { Icon } from "../../utils/icon.styles";
+
+export const Header = (): JSX.Element => {
+    return (
+        <>
+            <StyledHeader>
+                <LeftSection>
+                    <Icon className="menu">
+                        <SlMenu size={17} />
+                    </Icon>
+                    <LogoSection to="/">
+                        <FaYoutube color='FF0000' size={30} />
+                        <Text className="logo">YouTube</Text>
+                    </LogoSection>
+                </LeftSection>
+                <SearchSection>Search Section</SearchSection>
+                <HeaderMoreSection></HeaderMoreSection>
+            </StyledHeader>
+        </>
+    );
+};

ac506237e138ef9a74faf4aa172df42ae568d596
Angel <angel.pashov@proton.me>
2024-10-15 17:54:55 +0300

    Settings refactored to work with translations

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,42 +1,42 @@
-import { Setting, StyledSettings } from "./Settings.style";
+import { Setting, StyledSettings } from './Settings.style';
 import { HiLanguage } from "react-icons/hi2";
 import { GoMoon } from "react-icons/go";
 import { Text } from "../../utils/text.styles";
 import { useAppContext } from "../../context/App.context";
 
 interface SettingObjProps {
-    label: string;
-    icon: JSX.Element;
-    value: string;
-    onClick: () => void;
-};
+	label: string;
+	icon: JSX.Element;
+	value: string;
+	onClick: () => void;
+}
 
 export const Settings = (): JSX.Element => {
-    const { theme, toggleTheme } = useAppContext();
+	const { theme, language, toggleTheme, toggleLanguage, text } = useAppContext();
 
-    const SETTINGS = [
-        {
-            label: "Language",
-            icon: <HiLanguage size={23} />,
-            value: "English",
-            onClick: () => null,
-        },
-        {
-            label: "Appearance",
-            icon: <GoMoon size={23} />,
-            value: theme,
-            onClick: () => toggleTheme(),
-        }
-    ];
+	const SETTINGS = [
+		{
+			label: text.language,
+			icon: <HiLanguage size={23} />,
+			value: text[language === "english" ? "french" : "english"],
+			onClick: () => toggleLanguage(),
+		},
+		{
+			label: text.appearance,
+			icon: <GoMoon size={23} />,
+			value: text[theme === "light" ? "dark" : "light"],
+			onClick: () => toggleTheme(),
+		},
+	];
 
-    return (
-        <StyledSettings>
-            {SETTINGS.map(({ label, icon, value, onClick }: SettingObjProps) => (
-                <Setting key={label} onClick={onClick}>
-                    {icon}
-                    <Text>{`${label} : ${value}`}</Text>
-                </Setting>
-            ))}
-        </StyledSettings>
-    );
+	return (
+		<StyledSettings>
+			{SETTINGS.map(({ label, icon, value, onClick }: SettingObjProps) => (
+				<Setting key={label} onClick={onClick}>
+					{icon}
+					<Text>{`${label} : ${value}`}</Text>
+				</Setting>
+			))}
+		</StyledSettings>
+	);
 };

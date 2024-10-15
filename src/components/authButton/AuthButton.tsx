f77ed8659e1511d4bb9149d2553674170b9e294a
Angel <angel.pashov@proton.me>
2024-10-15 17:55:28 +0300

    Sign In text comes depending on the language

--- src/components/authButton/AuthButton.tsx
+++ src/components/authButton/AuthButton.tsx
@@ -1,12 +1,15 @@
 import { StyledAuthButton } from './AuthButton.style';
 import { FaUserCircle } from "react-icons/fa";
 import { Text } from '../../utils/text.styles';
+import { useAppContext } from '../../context/App.context';
 
 export const AuthButton = (): JSX.Element => {
+    const { text } = useAppContext();
+
     return (
         <StyledAuthButton>
             <FaUserCircle size={22} />
-            <Text className='auth'>Sign In</Text>
+            <Text className='auth'>{text.signIn}</Text>
         </StyledAuthButton>
     )
 }

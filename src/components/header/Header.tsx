eb4f01e220363db06764105b3a864a15ed093d70
Angel <angel.pashov@proton.me>
2024-10-16 14:42:56 +0300

    Search bar added

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,26 @@
 import { useState } from "react";
-import { FaYoutube } from "react-icons/fa";
-import { HeaderMoreSection, LeftSection, LogoSection, SearchSection, StyledHeader } from "./Header.styles";
+import { FaMicrophone, FaYoutube } from "react-icons/fa";
+import {
+    HeaderMoreSection,
+    LeftSection, LogoSection,
+    SearchBar,
+    SearchSection,
+    StyledHeader
+} from "./Header.styles";
 import { SlMenu } from "react-icons/sl";
 import { Text } from "../../utils/text.styles";
-import { Icon } from "../../utils/icon.styles";
+import { Icon } from '../../utils/icon.styles';
 import { AuthButton } from "../authButton/AuthButton";
 import { CgMoreVerticalAlt } from "react-icons/cg";
 import { Settings } from "../Settings/Settings";
+import { useAppContext } from "../../context/App.context";
+import { LuSearch } from "react-icons/lu";
 
 export const Header = (): JSX.Element => {
     const [showSettings, setShowSettings] = useState<boolean>(false);
+    const [searchText, setSearchText] = useState<string>('');
+
+    const { text } = useAppContext();
 
     return (
         <>
@@ -23,7 +34,18 @@ export const Header = (): JSX.Element => {
                         <Text className="logo">YouTube</Text>
                     </LogoSection>
                 </LeftSection>
-                <SearchSection>Search Section</SearchSection>
+                <SearchSection>
+                    <SearchBar>
+                        <input
+                            value={searchText}
+                            placeholder={text.search}
+                            onChange={e => setSearchText(e.target.value)}
+                        />
+                        <Icon>
+                            <LuSearch  size={21} />
+                        </Icon>
+                    </SearchBar>
+                </SearchSection>
                 <HeaderMoreSection>
 
                     <Icon data-tooltip-id="settings" data-tooltip-content="Settings" onClick={() => setShowSettings(currentState => !currentState)}>

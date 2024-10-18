fbca5c328585c776ef16ed72fda6e468c78ba939
Angel <angel.pashov@proton.me>
2024-10-18 19:23:03 +0300

    Added the homepage component

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,10 +1,12 @@
 import { Categories } from "../categories/Categories";
+import { HomepageVideos } from "../homepageVideos/HomepageVideos";
 import { StyledContent } from "./Content.styles";
 
 export const Content = (): JSX.Element => {
     return(
         <StyledContent>
             <Categories />
+            <HomepageVideos />
         </StyledContent>
     );
 };
---
+++ src/components/homepageVideos/HomepageVideos.tsx
@@ -0,0 +1,19 @@
+import { RegularVideoThumbnailsContainer, StyledHomepageVideos } from "./HomepageVideos.styles";
+import { useAppContext } from "../../context/App.context";
+import { RegularVideoItem } from "../regularVideoItem/RegularVideoItem";
+
+export const HomepageVideos = (): JSX.Element => {
+    const { videos: { videos } } = useAppContext();
+
+    const FIRST_VIDEO_SECTION = videos.splice(0, 8);
+    const SECOND_VIDEO_SECTION = videos.splice(8, 20);
+    const THIRD_VIDEO_SECTION = videos.splice(20, 28);
+
+    return (
+        <StyledHomepageVideos>
+            <RegularVideoThumbnailsContainer>
+                {FIRST_VIDEO_SECTION.map((video, index) => <RegularVideoItem video={video}/>)}
+            </RegularVideoThumbnailsContainer>
+        </StyledHomepageVideos>
+    );
+};

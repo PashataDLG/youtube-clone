7e7db5cc22635770d28364504b6e1d645dc626f7
Angel <angel.pashov@proton.me>
2024-10-21 13:53:57 +0300

    Added show less or more component for the shorts section

--- src/components/videoShorts/VideoShorts.styles.ts
+++ src/components/videoShorts/VideoShorts.styles.ts
@@ -15,3 +15,34 @@ export const VideoShortsHeading = styled.div`
         font-weight: bold;
     }
 `;
+
+export const MoreLessContainer = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    border-top: 1px solid ${({ theme }) => theme.divider};
+    margin-top: 2.5rem;
+    margin-bottom: .5rem;
+`;
+
+export const MoreLessButton = styled.button`
+    border: 1px solid ${({ theme }) => theme.divider};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: .5rem;
+    padding: .5rem 0rem;
+    width: 23.5rem;
+    background-color: ${({ theme }) => theme.background};
+    border-radius: 100rem;
+    transform: translateY(-50%);
+
+    &:hover{
+        background-color: ${({ theme }) => theme.grey2};
+        cursor: pointer;
+    };
+
+    .icon{
+        color: ${({ theme }) => theme.text};
+    }
+`;
--- src/components/videoShorts/VideoShorts.tsx
+++ src/components/videoShorts/VideoShorts.tsx
@@ -1,17 +1,34 @@
 import { Video } from "pexels";
-import { StyledVideoShorts, VideoShortsHeading } from "./VideoShorts.styles";
+import { MoreLessButton, MoreLessContainer, StyledVideoShorts, VideoShortsHeading } from "./VideoShorts.styles";
 import { SiYoutubeshorts } from "react-icons/si";
+import { Text } from "../../utils/text.styles";
+import { useAppContext } from "../../context/App.context";
+import { useState } from "react";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 interface IVideoShorts {
     videos: Video[]
 };
 
 export const VideoShorts = (props: IVideoShorts): JSX.Element => {
+    const [showLess, setShowLess] = useState<boolean>(true);
+    const { text } = useAppContext();
+
     return (
         <StyledVideoShorts>
             <VideoShortsHeading>
-            <SiYoutubeshorts size={25} color="red"/>
+                <SiYoutubeshorts size={25} color="red" />
+                <Text>{text.shorts}</Text>
             </VideoShortsHeading>
+            <MoreLessContainer>
+                <MoreLessButton onClick={() => setShowLess(state => !state)}>
+                    <Text>{showLess ? text.showMore : text.showLess}</Text>
+                    {showLess ?
+                        <IoIosArrowDown className="icon" size={20} />
+                        :
+                        <IoIosArrowUp className="icon" />}
+                </MoreLessButton>
+            </MoreLessContainer>
         </StyledVideoShorts>
     );
 }
